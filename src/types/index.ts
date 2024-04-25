52fefe7d95bc5d19e928a653f5ef249caebfeebd
JhonatanDzG <gizudajhovi@gmail.com>
2024-04-25 16:54:04 -0500

    feat: TodaysImage.tsx

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type PostImage = {
+    date?: string;
+    explanatio?: string;
+    hdurl?: string;
+    media_type?: string;
+    service_version?: string;
+    title?: string;
+    url?: string;
+}
--- src/views/Home.tsx/Home.tsx
+++ src/views/Home.tsx/Home.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
+import {format, sub} from "date-fns";
+
 import Header from '../../components/Header';
+import fetchAPI from '../../utils/fetch';
+import TodaysImage from '../../todaysImage/TodaysImage';
+import { PostImage } from '../../types/index';
 
 const Home = () => {
+  const [todaysImage, setTodaysImage] = useState<PostImage>({});
+ 
+  useEffect(() => {
+    const loadActualImage = async () => {
+      try {
+        const actualImage = await fetchAPI();
+        setTodaysImage(actualImage)
+
+      } catch (error) {
+        console.error(error);
+        setTodaysImage({});
+      }
+    };
+
+    const loadLast5DaysImage = async () => {
+      try{
+        const date = new Date();
+        const todaysDate = format(date, "yyyy-MM-dd")
+        const fiveDaysAgoDate = format(sub(date, {days: 5}), "yyyy-MM-dd")
+      }catch(error){
+        console.error(error);
+      }
+    }
+
+    loadActualImage().catch(null);
+    loadLast5DaysImage().catch(null);
+
+  }, []);
+
+console.log( todaysImage);
+console.log("### IMAGE DATA END ###");
+
+
   return (
     <View style={styles.container}>
       <Header />
+      <TodaysImage {...todaysImage}/>
     </View>
   );
 };
@@ -15,6 +54,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
   },
-
 });
 export default Home;
